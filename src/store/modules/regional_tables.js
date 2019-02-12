13ec80db59c8ac9291403e88c768ee6cebae399f
Kris <blitzkris24@gmail.com>
2019-02-12 14:54:24 +0800

    admin

---
+++ src/store/modules/regional_tables.js
@@ -0,0 +1,67 @@
+import ProductType from '../../api/apiRegional';
+
+const state = {
+
+    region: []
+
+}
+
+const mutations = {
+    SET_REGION(state, data) {
+        state.region = data
+        console.log("###PREGION:SET###" + JSON.stringify(data))
+    }
+
+}
+
+var actions = {
+
+    // REGION
+    GET_REGION(context) {
+        return new Promise((resolve, reject) => {
+            new RegionType(context.rootState.user_session.token).getRegion((err, data) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    context.commit('SET_REGION', data)
+                    resolve(data)
+                }
+            })
+        })
+    },
+    ADD_REGION(context, new_region) {
+        return new Promise((resolve, reject) => {
+            new RegionType(context.rootState.user_session.token).addRegion(new_region, (err, data) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+
+    },
+    EDIT_PRODUCTS(context, modified_region) {
+        return new Promise((resolve, reject) => {
+            new RegionType(context.rootState.user_session.token).editRegion(modified_region, (err, data) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+
+    },
+
+    //PROVINCE
+
+    //CITY
+
+}
+
+export default {
+    state,
+    mutations,
+    actions
+}

1e9d5b5d41cc91fddf7ad9339c2ca72ac18ba4f8
AntonioJr A. Basas <Antonio A. Basas, Jr>
2019-05-29 09:00:16 +0800

    Admin

--- src/api/apiCountryOriginManager.js
+++ src/api/apiCountryOriginManager.js
@@ -11,7 +11,7 @@ export default class CountryOriginType {
 
     getCountryOrigin(cb) {
 
-        axios.get('core/origin').then((result) => {
+        axios.get('core/origins').then((result) => {
                 console.log("###API### GET CountryOrigin")
                 if (result.data.success) {
                     cb(result.data.errors, result.data.model)
@@ -26,7 +26,7 @@ export default class CountryOriginType {
 
     addCountryOrigin(new_country_origin, cb) {
         console.log("api############# add countryorigin" + JSON.stringify(new_country_origin))
-        axios.post('core/origin', new_country_origin).then((result) => {
+        axios.post('core/origins', new_country_origin).then((result) => {
                 console.log("api############# ADD CountryOrigin")
                 cb(result.data.errors, result.data.model)
             })
@@ -37,7 +37,7 @@ export default class CountryOriginType {
 
     editCountryOrigin(modified_country_origin, cb) {
         var id = modified_country_origin._id
-        axios.post('core/origin/' + id, modified_country_origin).then((result) => {
+        axios.post('core/origins/' + id, modified_country_origin).then((result) => {
                 console.log("api############# Edit CountryOrigin" + JSON.stringify(modified_country_origin))
                 cb(result.data.errors, result.data.model)
             })
